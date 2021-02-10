768292e43f70b28619debb40017d7ed493626b0f
Jena Boehm <boehmjena@gmail.com>
2021-02-09 18:05:44 -0800

    tests passing yay

--- src/components/list/tests/DogList.test.jsx
+++ src/components/list/tests/DogList.test.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import DogList from '../DogList';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('DogList component', () => {
   afterEach(() => cleanup());
   it('renders DogList', () => {
-    const { asFragment } = render(<DogList />);
+    const { asFragment } = render(
+      <MemoryRouter>
+        <DogList dogs={[]} />
+      </MemoryRouter>);
     expect(asFragment()).toMatchSnapshot();
   });
 });

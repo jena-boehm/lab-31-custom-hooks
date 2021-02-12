cb558f693ee2f2b9103d35886401af52ab9ac45f
Jena Boehm <boehmjena@gmail.com>
2021-02-12 13:12:28 -0800

    tests passing

--- src/components/header/tests/Header.test.jsx
+++ src/components/header/tests/Header.test.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import Header from '../Header';
 import { MemoryRouter } from 'react-router-dom';
+import { ThemeProvider } from '../../../state/themeContext';
 
 describe('Header component', () => {
   afterEach(() => cleanup());
   it('renders Header', () => {
     const { asFragment } = render(
-      <MemoryRouter>
-        <Header />
-      </MemoryRouter>);
+      <ThemeProvider>
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>
+      </ThemeProvider>);
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- src/components/list/tests/DogList.test.jsx
+++ src/components/list/tests/DogList.test.jsx
@@ -3,14 +3,17 @@ import { render, cleanup } from '@testing-library/react';
 import DogList from '../DogList';
 import { MemoryRouter } from 'react-router-dom';
 import dogs from '../../../fixtures/dogs.json';
+import { ThemeProvider } from '../../../state/themeContext';
 
 describe('DogList component', () => {
   afterEach(() => cleanup());
   it('renders DogList', () => {
     const { asFragment } = render(
-      <MemoryRouter>
-        <DogList dogs={[dogs]} />
-      </MemoryRouter>);
+      <ThemeProvider>
+        <MemoryRouter>
+          <DogList dogs={[dogs]} />
+        </MemoryRouter>
+      </ThemeProvider>);
     expect(asFragment()).toMatchSnapshot();
   });
 });
